44c46f9597868c885bd7b5c8a2fb26456c05f4a7
hillarymuir <hillarymuir@pm.me>
2025-12-08 18:30:00 -0500

    10: allow user input of book file to be analyzed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import word_count, char_count, generate_report_list
+import sys
 
 # read book text from file into a str
 def get_book_text(filepath):
@@ -6,7 +7,10 @@ def get_book_text(filepath):
         return f.read()
 
 def main():
-    book_location = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_location = sys.argv[1]
     book_text = get_book_text(book_location)
     char_count_dict = char_count(book_text)
     report_list = generate_report_list(char_count_dict)
